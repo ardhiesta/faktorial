ad369606b1c8b089b753869dc6d3c3db4951279c
Julian Dewanto <juliandewanto@student.uns.ac.id>
2022-12-02 01:00:27 +0700

    Menambahkan code untuk menghitung nilai faktorial

--- src/App.java
+++ src/App.java
@@ -1,50 +1,67 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import java.awt.*;
 
-public class App {
-    static JTextField jtfInput;
-    static JButton jBtnHitung;
-    static JLabel jlHasil;
+static JTextField jtfInput;
+static JButton jBtnHitung;
+static JLabel jlHasil;
+static JButton btn = new JButton();
+
+public static void main(String[] args) throws Exception {
+    
+    // membuat frame / window
+    JFrame jfFrame = new JFrame();
 
     public static void main(String[] args) throws Exception {
-        // membuat frame / window
-        JFrame jfFrame = new JFrame();
-
-        // set window title 
-        jfFrame.setTitle("Program sederhana hitung faktorial");
-        jfFrame.setSize(500, 180);
-        jfFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // menambahkan komponen JPanel
-        JPanel jPanel = new JPanel();
-        // mengatur layout JPanel, komponen akan disusun tampil secara vertikal
-        jPanel.setLayout(new GridLayout(3,1));
-
-        // membuat objek JTextField untuk menampung input 
-        jtfInput = new JTextField(10);
-        jtfInput.setText("");
-        // menambahkan objek JTextField ke dalam JPanel
-        jPanel.add(jtfInput);
-
-        // membuat objek JButton untuk menghitung faktorial 
-        jBtnHitung = new JButton();
-        // mengatur text dalam button
-        jBtnHitung.setText("Hitung Faktorial");
-        // menambahkan objek JButton ke dalam JPanel
-        jPanel.add(jBtnHitung);
-
-        // membuat objek JTextField untuk menampilkan hasil faktorial 
-        jlHasil = new JLabel();
-        // mengatur text yang tampil di label
-        jlHasil.setText("hasil faktorial : ");
-        jPanel.add(jlHasil);
-
-        // menambahkan jPanel ke JFrame
-        jfFrame.add(jPanel);
-        jfFrame.setVisible(true);
+    
+            // menambahkan komponen JPanel
+            JPanel jPanel = new JPanel();
+    
+            // mengatur layout JPanel, komponen akan disusun tampil secara vertikal
+            jPanel.setLayout(new GridLayout(3,1));
+            jPanel.setLayout(new GridLayout(4,1));
+    
+            // membuat objek JTextField untuk menampung input 
+            jtfInput = new JTextField(10);
+            jtfInput.setText("");
+    
+            // menambahkan objek JTextField ke dalam JPanel
+            jPanel.add(jtfInput);
+    
+            // membuat objek JButton untuk menghitung faktorial 
+            jBtnHitung = new JButton();
+    
+            // mengatur text dalam button
+            jBtnHitung.setText("Hitung Faktorial");
+    
+            // menambahkan objek JButton ke dalam JPanel
+            jPanel.add(jBtnHitung);
+    
+            //Mengatur action saat menekan button
+            jBtnHitung.addActionListener(new ActionListener(){
+                private long Faktorial() {
+                    int x = Integer.parseInt(jtfInput.getText());
+                    int i, fact=1;  
+                    for(i=1;i<=x;i++){    
+                        fact=fact*i;    
+                    }    
+                    return fact;
+    
+                }
+                public void actionPerformed(ActionEvent e){ 
+                    long result = Faktorial();
+                    jlHasil.setText(String.valueOf(result));  
+                }
+    
+            });
+    
+            // membuat objek JTextField untuk menampilkan hasil faktorial 
+            jlHasil = new JLabel();
+    
+            // mengatur text yang tampil di label
+            jlHasil.setText("hasil faktorial : ");
+            jPanel.add(jlHasil);
+        }
     }
-}
